e986d5c1c408a74f5c0707e0a2962889463019a7
JedilsonSilva <jedilsondemelo@gmail.com>
2026-02-07 21:13:58 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,19 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+// AAA - Arrange, Act, Assert
+/// Preparar, Agir, Verificar
 
-  // Checkpoint
+test('deve consultar um pedido aprovado', async ({ page }) => {
+    // Arrange
+  await page.goto('http://localhost:5173/');
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  // Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+    // Act
   await page.getByTestId('search-order-id').fill('VLO-512JKF');
-
   await page.getByTestId('search-order-button').click();
-  
+
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-512JKF');
 
