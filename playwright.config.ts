6b02e2acad1d46b546060487505bae37069d53bf
jedilsonsilva <jedilsondemelo@gmail.com>
2026-09-13 21:37:11 -0300

    estagio de execução dos testes de regressão

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-jedi.vercel.app',
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
@@ -94,7 +94,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'yarn dev',
-    url: 'https://velo-jedi.vercel.app',
+    url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
     timeout: 120_000,
   },
