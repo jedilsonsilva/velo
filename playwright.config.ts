067bd0938e9a61e8c02686859d7be6e70025d255
jedilsonsilva <jedilsondemelo@gmail.com>
2026-09-13 22:29:13 -0300

    estagio de execução dos testes de regressão

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
     baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on',
 
     // Tempo máximo para ações interativas como click(), fill()
     // Quando o valor é 0, herda o limite do timeout geral do teste
