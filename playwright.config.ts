876922d4880e92ff59efb0212d89d3ffc0ea6646
jedilsonsilva <jedilsondemelo@gmail.com>
2026-09-14 21:57:35 -0300

    Adicionando integração com o TestDino

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,14 +31,20 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    // Optional, enables native HTML upload
+    ['html', { outputDir: './playwright-report' }],
+    // Mandatory reporter for JSON results
+    ['json', { outputFile: './playwright-report/report.json' }],
+    //['@testdino/playwright', { token: process.env.TESTDINO_TOKEN }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'on-first-retry',
 
     // Tempo máximo para ações interativas como click(), fill()
     // Quando o valor é 0, herda o limite do timeout geral do teste
